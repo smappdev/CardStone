6492af2647d75cc910be5b06e7ea3db70fa3f0c2
smappdev <saul.appdev@gmail.com>
2017-02-17 12:19:05 +0100

    Added picasso gson and volley libraries. Created entity classes

---
+++ app/src/main/java/com/saul/cardstone/model/entities/CardResponseEntity.java
@@ -0,0 +1,12 @@
+package com.saul.cardstone.model.entities;
+
+/**
+ * Created by Saul on 17/02/2017.
+ */
+
+public class CardResponseEntity {
+
+
+
+
+}
---
+++ app/src/main/java/com/saul/cardstone/model/entities/CardResultsEntity.java
@@ -0,0 +1,8 @@
+package com.saul.cardstone.model.entities;
+
+/**
+ * Created by Saul on 17/02/2017.
+ */
+
+public class CardResultsEntity {
+}
